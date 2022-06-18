ae5ed6450a2d2a9f698aacc7d8a411de72c2da2b
6hartanto <6hartanto@gmail.com>
2022-06-18 22:09:10 +0700

    style: styling note list

--- src/components/NoteBody.js
+++ src/components/NoteBody.js
@@ -1,12 +1,30 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function NoteBody({ title, body }) {
   return (
-    <div className="note-body">
-      <h3>{title}</h3>
-      <p>{body}</p>
-    </div>
+    <Card
+      sx={{ maxWidth: 345, margin: '8px' }}
+      variant="outlined"
+    >
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {body}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 }
 
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,19 +1,34 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import Container from '@mui/material/Container';
+import Header from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import NoteBody from './NoteBody';
 
 // eslint-disable-next-line react/prop-types
 function NoteList({ notes }) {
   return (
-    <div className="note-list">
-      {
-      // eslint-disable-next-line react/prop-types
-      notes.map((note) => (
-        <NoteBody key={note.id} {...note} />
-      ))
-      }
-    </div>
+    <Container>
+      <Header variant="h5" component="h5">
+        Catatan Aktif
+      </Header>
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        justifyContent="space-between"
+        sx={{
+          maxWidth: '100%',
+        }}
+      >
+        {
+          // eslint-disable-next-line react/prop-types
+          notes.map((note) => (
+            <NoteBody key={note.id} {...note} />
+          ))
+        }
+      </Box>
+    </Container>
   );
 }
 

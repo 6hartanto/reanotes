e3125e2eeadd7c69ed2c787af2334a4ea706ea0a
6hartanto <6hartanto@gmail.com>
2022-06-19 00:13:53 +0700

    style: styling delete button

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { Button } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function DeleteButton({ id, onDelete }) {
-  return <button type="button" onClick={() => onDelete(id)}>Delete</button>;
+  return <Button variant="outlined" color="error" startIcon={<DeleteIcon />} onClick={() => onDelete(id)}>Delete</Button>;
 }
 
 export default DeleteButton;
